90a57d1bdd22acfaa8143bd04a2eefec751e4f1b
rafidharyu <rafidharyu313@gmail.com>
2024-11-19 10:54:20 +0700

    memperbaikin MainController

--- app/Http/Controllers/Frontend/MainController.php
+++ app/Http/Controllers/Frontend/MainController.php
@@ -4,9 +4,8 @@ namespace App\Http\Controllers\Frontend;
 
 use App\Http\Controllers\Controller;
 use App\Http\Services\CarService;
-
 use Illuminate\Http\Request;
-use App\Http\Controllers\Controller;
+
 
 class MainController extends Controller
 {
@@ -18,45 +17,57 @@ class MainController extends Controller
     public function index()
     {
         return view('frontend.index',[
-            'cars' => $this->carService->select(10)
+            'cars' => $this->carService->select()
         ]);
     }
 
     public function car()
     {
         return view('frontend.car',[
-            'cars' => $this->carService->select(10)
+            'cars' => $this->carService->select()
         ]);
     }
 
     public function about()
     {
-        return view('frontend.about');
+        return view('frontend.about',[
+            'cars' => $this->carService->select()
+        ]);
     }
 
     public function service()
     {
-        return view('frontend.service');
+        return view('frontend.service',[
+            'cars' => $this->carService->select()
+        ]);
     }
 
     public function blog()
     {
-        return view('frontend.blog');
+        return view('frontend.blog',[
+            'cars' => $this->carService->select()
+        ]);
     }
 
     public function contact()
     {
-        return view('frontend.contact');
+        return view('frontend.contact',[
+            'cars' => $this->carService->select()
+        ]);
     }
 
     public function feature()
     {
-        return view('frontend.feature');
+        return view('frontend.feature',[
+            'cars' => $this->carService->select()
+        ]);
     }
 
     public function testimonial()
     {
-        return view('frontend.testimonial');
+        return view('frontend.testimonial',[
+            'cars' => $this->carService->select()
+        ]);
     }
 
 
